559464bfaef84eba9579b7e016e46ef6e473a457
Jabed <jabedomorbappi100@gmail.com>
2024-08-03 17:16:30 +0600

    create fragment

---
+++ app/src/main/java/com/jabed/navigation/SecondFragment.kt
@@ -0,0 +1,36 @@
+package com.jabed.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.jabed.navigation.databinding.FragmentSecondBinding
+
+
+class SecondFragment : Fragment() {
+    private lateinit var binding:FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+
+        binding=FragmentSecondBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btn2.setOnClickListener {
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+
+    }
+
+
+}
